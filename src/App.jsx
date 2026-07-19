c40d9cf31529f4c13abe465601b43e0d67c119a2
Abolfazl Hosseini <tryuzr@gmail.com>
2026-07-19 04:20:24 +0330

    Replace BrowserRouter with HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import CategoryRouteHandler from "./components/CategoryRouteHandler";
 import Toast from "./components/UI/Toast";
 
